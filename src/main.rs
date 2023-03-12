5f667c6c349777b20556826f43424ab68861f0d0
NotAYoutuberYT <NotAYoutuberYT>
2023-03-12 14:30:26 -0600

    cleanup main

--- src/main.rs
+++ src/main.rs
@@ -13,13 +13,19 @@ use serenity::{
 mod bot_token;
 use bot_token::BOT_TOKEN;
 
+//
 // local modules
+//
+
 mod extract_from_command;
 
 mod todos;
 use todos::todo::Todo;
 
+//
 // bot commands
+//
+
 mod groups;
 use groups::{create_group, group::Group, list_groups};
 
@@ -28,7 +34,10 @@ use privileged_role_config::{
     admin_list::AdminRoleList, give_admin, list_privileged_roles, remove_admin,
 };
 
+//
 // bot persistent data
+//
+
 struct AdminRoles;
 impl TypeMapKey for AdminRoles {
     type Value = AdminRoleList;
@@ -39,42 +48,14 @@ impl TypeMapKey for Groups {
     type Value = Vec<Group>;
 }
 
+//
+// Handler implementation
+//
+
 struct Handler;
 
 #[async_trait]
 impl EventHandler for Handler {
-    // slash command handler
-    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
-        if let Interaction::ApplicationCommand(command) = interaction {
-            // quick console log
-            println!("{} invoked {}", command.user.name, command.data.name);
-
-            // run the command and store the result
-            let result: Result<(), Error> = match command.data.name.as_str() {
-                "give-elevated-privileges" => give_admin::run(&command, &ctx).await,
-                "remove-privileges" => remove_admin::run(&command, &ctx).await,
-                "list-privileged-roles" => list_privileged_roles::run(&command, &ctx).await,
-                "create-group" => create_group::run(&command, &ctx).await,
-                "list-groups" => list_groups::run(&command, &ctx).await,
-                _ => Err(Error::Other("Command not implemented")),
-            };
-
-            // if an error occured, log it
-            if let Err(e) = result {
-                command
-                    .create_interaction_response(&ctx.http, |response| {
-                        response
-                            .kind(InteractionResponseType::ChannelMessageWithSource)
-                            .interaction_response_data(|message| {
-                                message.content("Sorry, an error occured. Ask the person hosting the bot to check the logs!")
-                            })
-                    })
-                    .await.unwrap_or_default();
-                println!("Error when running command {}: {:?}", command.data.name, e)
-            }
-        }
-    }
-
     // shard start handler
     async fn ready(&self, ctx: Context, ready: Ready) {
         // create the slash commands using
@@ -98,6 +79,38 @@ impl EventHandler for Handler {
             ready.user.name,
             ready.user.id,
         );
+    }
+
+    // slash command handler
+    async fn interaction_create(&self, ctx: Context, interaction: Interaction) {
+        if let Interaction::ApplicationCommand(command) = interaction {
+            // quick console log
+            println!("{} invoked {}", command.user.name, command.data.name);
+
+            // run the command and store the result
+            let result: Result<(), Error> = match command.data.name.as_str() {
+                "give-elevated-privileges" => give_admin::run(&command, &ctx).await,
+                "remove-privileges" => remove_admin::run(&command, &ctx).await,
+                "list-privileged-roles" => list_privileged_roles::run(&command, &ctx).await,
+                "create-group" => create_group::run(&command, &ctx).await,
+                "list-groups" => list_groups::run(&command, &ctx).await,
+                _ => Err(Error::Other("Command not implemented")),
+            };
+
+            // if an error occurred, log it
+            if let Err(e) = result {
+                command
+                    .create_interaction_response(&ctx.http, |response| {
+                        response
+                            .kind(InteractionResponseType::ChannelMessageWithSource)
+                            .interaction_response_data(|message| {
+                                message.content("Sorry, an error occurred. Ask the person hosting the bot to check the logs!")
+                            })
+                    })
+                    .await.unwrap_or_default();
+                println!("Error when running command {}: {:?}", command.data.name, e)
+            }
+        }
     }
 }
 
